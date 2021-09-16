99ed260882b54645c27c440a33738a0d9aeded66
Luana Souza <87820972+LuanaSVieira@users.noreply.github.com>
2021-09-16 18:41:20 -0300

    Add files via upload

---
+++ atv1.js
@@ -0,0 +1,68 @@
+//atividade 1 - variáveis com tipos de dados
+
+//Number
+
+var variavelNumber = 8;;
+
+console.log(variavelNumber);
+
+var numeroNegativo = -14;
+
+console.log(numeroNegativo);
+
+//String
+
+var aspasSimples = 'Crescer é um processo';
+
+console.log(aspasSimples);
+
+var aspasDupla = "O melhor está por vir.";
+
+console.log(aspasDupla);
+
+//Boolean
+
+var varVerdade = true;
+var varMentira = false;
+
+console.log("É verdade que você está estudando?");
+
+console.log(varVerdade);
+
+console.log("Você está com preguiça?");
+
+console.log(varMentira);
+
+//Array
+
+var paises = ["Itália", "Brasil", "Portugal"];
+
+console.log(paises);
+
+var cores = ["azul", "amarelo", "vermelho"];
+
+console.log(cores);
+
+//object
+
+var pessoa = {nome: "Luana", idade: "19", nacionalidade: "brasileira"};
+
+console.log(pessoa);
+
+console.log(pessoa.idade);
+
+//Undefined 
+
+var semValor;
+
+console.log("semValor: " + semValor);
+
+//null
+
+var valorVazio = null;
+
+console.log("valorVazio: " + valorVazio);
+
+
+
+
---
+++ atv2.js
@@ -0,0 +1,29 @@
+//variáveis usando a palavra-chave var
+
+var nomeEscola = "EREM Rodolfo";
+
+console.log(nomeEscola);
+
+var  nomeAluno = "Antony";
+
+console.log(nomeAluno);
+
+//variáveis usando a palavra-chave let
+
+let nota_unidade1 = 7;
+
+console.log(nota_unidade1);
+
+let nota_unidade2 = 6;
+
+console.log(nota_unidade2);
+
+//constantes usando a palavra-chave const
+
+const mediaEscolar = 6;
+
+console.log(mediaEscolar);
+
+const valorPI = 3.14;
+
+console.log(valorPI);
